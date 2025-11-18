08f3c3836708fea4a18667a48afef988a553af4d
vcnt <thiago.vicente@ua.pt>
2025-11-18 14:53:13 +0000

    Files

---
+++ src/main/java/com/example/OLSHEETS/OlsheetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.OLSHEETS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlsheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlsheetsApplication.class, args);
+	}
+
+}
